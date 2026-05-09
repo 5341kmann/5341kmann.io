527430601305226b45f9a43643bf452b62f7a5ff
[Grant Sackmann]] <[grant.sackmann@icloud.com]>
2026-05-08 22:25:00 -0500

    added competencies page, button redirect to competencies page, updated header, and vite configuration file

---
+++ config.vite.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  base: "/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        projects: resolve(__dirname, "competencies.html"),
+      },
+    },
+  },
+});
