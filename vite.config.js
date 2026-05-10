13f8e03ec76c50e762cb5aef52d55eb3b0f3d44c
[Grant Sackmann]] <[grant.sackmann@icloud.com]>
2026-05-10 01:43:04 -0500

    prismjs syntax highlighting for code examples in competency accordion

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
 import { dirname, resolve } from "node:path";
+import prismjs from "vite-plugin-prismjs"
 
 export default defineConfig({
+  plugins:[
+    prismjs({
+      languages: ["python"],
+      plugins: ['line-numbers', 'show-language'],
+      css: true,
+    }),
+  ],
   build: {
     rollupOptions: {
       input: {
