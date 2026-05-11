81d82c41ca18cdc10bf2092cdfcbc98083516489
[Grant Sackmann]] <[grant.sackmann@icloud.com]>
2026-05-10 20:29:52 -0500

    Fixed formatting of descriptions in competency accordion, updated css for syntax highlighting inline

--- vite.config.js
+++ vite.config.js
@@ -3,20 +3,17 @@ import { dirname, resolve } from "node:path";
 import prismjs from "vite-plugin-prismjs"
 
 export default defineConfig({
-  plugins:[
+  plugins: [
     prismjs({
       languages: ["python"],
-      plugins: ['line-numbers', 'show-language'],
+      plugins: ["line-numbers", "show-language"],
     }),
   ],
   build: {
     rollupOptions: {
       input: {
         main: resolve(import.meta.dirname, "index.html"),
-        pages: resolve(
-          import.meta.dirname,
-          "pages/CSC-324/competencies.html",
-        ),
+        pages: resolve(import.meta.dirname, "pages/CSC-324/competencies.html"),
       },
     },
   },
